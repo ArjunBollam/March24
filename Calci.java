cbd25986c4f634bc18dd08e1c7fbb4647b962570
subin77 <subin.shahukhal@gmail.com>
2017-03-24 13:57:40 -0500

    Update Calci.java

--- Calci.java
+++ Calci.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Calci {
 	int a;
 	int b;
-	int ans=1;
+	//int ans=1;
 	void power(int a,int b) throws NegativeException{
 		if (a<0 || b<0) {
 			throw new NegativeException("a and b should be non-negative");
