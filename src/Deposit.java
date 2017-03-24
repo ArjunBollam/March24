b41a2226dc07807416db10955c496f9bb84c586b
Subash Malla <emsuwas@gmail.com>
2017-03-24 12:13:15 -0500

    changes made again

--- src/Deposit.java
+++ src/Deposit.java
@@ -2,19 +2,19 @@
 public class Deposit extends Thread {
 
 	public Integer balance =0;
-	public Integer MAX_HOLD = 500;
+	public Integer MAX_HOLD = 600;
 	
 	
 	private void Deposit(int amount) throws InterruptedException {
 		synchronized(balance)
 		{
-		while(balance == 500)
+		while(balance == 600)
 		{
 			Thread.currentThread().setName("Deposit");
 			System.out.println("Account is full"+Thread.currentThread().getName() + " is waiting , balance: " +balance);
 			balance.wait();
 		}
-		Thread.sleep(10000);
+		Thread.sleep(100);
 		balance += amount;
 	  System.out.println("Total Balance after adding:"+ balance);
 	  balance.notify();
