0648d9de95255913f89f1e2785f3b1a9c9143678
kacbin <kacbin@gmail.com>
2017-03-24 14:15:50 -0500

    Update BankOfAmerica.java

--- BankOfAmerica.java
+++ BankOfAmerica.java
@@ -3,7 +3,7 @@ public class BankOfAmerica extends Banking{
 	public static void main(String[] args) {
 		BankOfAmerica object=new BankOfAmerica();
 		object.deposit(50000);
-		object.withdraw(10000);
+		//object.withdraw(10000);
 	}
 
 }
